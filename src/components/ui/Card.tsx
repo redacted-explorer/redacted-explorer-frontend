59a265a50f9f6d3a233745f6fe67179a3852c05b
Slime <4sliman4@gmail.com>
2024-11-11 22:19:36 +0200

    Delete Card

--- src/components/ui/Card.tsx
+++
@@ -1,7 +0,0 @@
-export default function Card({ children }: { children: React.ReactNode[] }) {
-  return (
-    <div className="soft-shadow rounded-xl w-full min-w-[360px] font-light ring-1 ring-near-green-200 divide-y divide-near-green-200">
-      {children.map((child) => child)}
-    </div>
-  );
-}
